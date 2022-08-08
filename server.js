bed255db100ae8e2078db7f26cb0e47d478ffc1b
fuentesjulian <fuentes.julian@hotmail.com>
2022-08-07 21:45:43 -0300

    Agregando comentarios en el codigo para explicar funcionalidad

--- server.js
+++ server.js
@@ -10,16 +10,24 @@ const productosRouter = new Router();
 productosRouter.use(express.json());
 productosRouter.use(express.urlencoded({ extended: true }));
 
+// si no define el id, obtiene todos los items con el metodo listarAll de la api
 productosRouter.get("/", (req, res) => {
   res.send(productosApi.listarAll());
 });
+
+// toma el id en la ruta desde params y corre el metodo listar de la api
+// el api checkea que el id existe, si no existe devuelve error
 productosRouter.get("/:id", (req, res) => {
   res.send(productosApi.listar(parseInt(req.params.id)));
 });
+
+// con el post agrega un producto
 productosRouter.post("/", (req, res) => {
   const id = productosApi.guardar(req.body);
   res.send(productosApi.listar(id));
 });
+
+// con el put modifica un producto existente (el api checkea que exista, si no existe devuelve error)
 productosRouter.put("/:id", (req, res) => {
   // obtengo el id
   const { id } = req.params;
@@ -28,12 +36,15 @@ productosRouter.put("/:id", (req, res) => {
   // utilizo el listar
   res.send(productosApi.listar(id));
 });
+
+// el comando delete toma el id de los parametros del request y elimina el item con dicho id
 productosRouter.delete("/:id", (req, res) => {
   const msg = productosApi.borrar(parseInt(req.params.id));
   res.send(msg);
 });
-// creo el server
 
+
+// creo el server
 const app = express();
 app.use(express.static("public"));
 app.use("/api/productos", productosRouter);
@@ -42,4 +53,6 @@ const PORT = 8080;
 const server = app.listen(PORT, () => {
   console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
 });
+
+// msg de error del servidor
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
